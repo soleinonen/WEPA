1cad338c849aa371d98362f82f57dadbcc17e40b
Sami Leinonen <soleinonen@gmail.com>
2020-10-14 21:13:27 +0300

    Fix to logout issues in Heroku.

--- src/main/java/wepa/controller/DefaultController.java
+++ src/main/java/wepa/controller/DefaultController.java
@@ -1,7 +1,13 @@
 package wepa.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +21,15 @@ public class DefaultController {
     @GetMapping("*")
     public String defaultController(Model model) {
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logOut(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login";
     }
 
 }
