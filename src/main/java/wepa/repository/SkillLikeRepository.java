ee926ea4ee8ff9f9cc97969a2d138e01772827ff
Sami Leinonen <soleinonen@gmail.com>
2020-11-04 17:17:48 +0200

    Try to fix Heroku error of skill like query.

--- src/main/java/wepa/repository/SkillLikeRepository.java
+++ src/main/java/wepa/repository/SkillLikeRepository.java
@@ -10,6 +10,6 @@ import wepa.model.SkillLike;
 
 public interface SkillLikeRepository extends JpaRepository<SkillLike, Long>{
 
-    @Query(value="SELECT new wepa.repository.SkillLikesDto(sl.skill.skillName, count(sl) as likeCount, sl.skill.id)  FROM SkillLike sl WHERE SKILL_OWNER_ID = :skillId GROUP BY sl.skill.skillName ORDER BY sl.skill DESC")
+    @Query(value="SELECT new wepa.repository.SkillLikesDto(sl.skill.skillName, count(sl) as likeCount, sl.skill.id)  FROM SkillLike sl WHERE SKILL_OWNER_ID = :skillId GROUP BY sl.skill.id, sl.skill.skillName ORDER BY likeCount DESC")
     List<SkillLikesDto> findSkillLikeByOwner(@Param("skillId") Long skillId);
 }
